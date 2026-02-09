4c9da915831c373186804e849875b3c3492a8b93
Slozzyondul <solomonondula@gmail.com>
2026-02-09 16:47:28 +0300

    scalar types

---
+++ learning_project/src/bin/scalar_values.rs
@@ -0,0 +1,34 @@
+#![allow(unused)]
+
+fn main() {
+    //scalar types
+    // - single value
+    // - building blocks for more complex types
+    // integers
+    // - signed integers
+    //range -(2^(n-1)) to (2^(n-1)-1)
+    // take the form of i8, i16, i32, i64, i128, isize
+    // i8 - 8-bit signed integer
+    // i16 - 16-bit signed integer
+    // i32 - 32-bit signed integer
+    // i64 - 64-bit signed integer
+    // i128 - 128-bit signed integer
+    // isize - pointer-sized signed integer
+    // - unsigned integers
+    //range 0 to (2^n - 1)
+    // take the form of u8, u16, u32, u64, u128, usize
+    // u8 - 8-bit unsigned integer
+    // u16 - 16-bit unsigned integer
+    // u32 - 32-bit unsigned integer
+    // u64 - 64-bit unsigned integer
+    // u128 - 128-bit unsigned integer
+    // usize - pointer-sized unsigned integer
+    // floats
+    // - take the form of f32, f64
+    // f32 - 32-bit floating point number
+    // f64 - 64-bit floating point number
+    // booleans
+    // - take the form of bool
+    // characters
+    // - take the form of char
+}
---
+++ learning_project/target/debug/deps/scalar_values-d17fba968675e40a.d
@@ -0,0 +1,5 @@
+/home/solo/documents/rust-projects-/learning_project/target/debug/deps/scalar_values-d17fba968675e40a.d: src/bin/scalar_values.rs
+
+/home/solo/documents/rust-projects-/learning_project/target/debug/deps/libscalar_values-d17fba968675e40a.rmeta: src/bin/scalar_values.rs
+
+src/bin/scalar_values.rs:
