642d52a2255d322a980bac3afaf67c7f1d52d190
Slozzyondul <solomonondula@gmail.com>
2026-02-09 16:03:31 +0300

    mutable and constants

--- learning_project/src/main.rs
+++ learning_project/src/main.rs
@@ -1,7 +1,16 @@
 fn main() {
     println!("Hello, world!");
 
-    //variables are mutable by defaullt in rust
+    //variables are imutable by defaullt in rust
     let x: i32 = 5;
     println!("The value of x is: {}", x);
+
+    //mutable variables
+    let mut y: i32 = 6;
+    y += 1;
+    println!("The value of y is: {}", y);
+
+    //constants
+    const MAX_POINTS: u32 = 100_000;
+    println!("The value of MAX_POINTS is: {}", MAX_POINTS);
 }
