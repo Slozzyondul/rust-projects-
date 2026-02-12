3adf4c25c3706070a554d455ba814a41ac3b66c3
Slozzyondul <solomonondula@gmail.com>
2026-02-12 13:50:11 +0300

    working with enums

--- learning_project/src/bin/enum.rs
+++ learning_project/src/bin/enum.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 
+#[derive(Debug, PartialEq)]
 enum Color {
     Red,
     Green,
@@ -12,7 +13,40 @@ enum Color {
 fn main() {
     // enum
 
-    // attributes - debug and partialeq
+    // attributes - debug
+    let red = Color::Red;
+    let rgba = Color::Rgba(255, 0, 0, 1.0);
+    let hex = Color::Hex("#FF0000".to_string());
+    let hsl = Color::Hsl {
+        h: 0,
+        s: 100,
+        l: 50,
+    };
+
+    println!("{:?}", red);
+    println!("{:?}", rgba);
+    println!("{:?}", hex);
+    println!("{:?}", hsl);
+
+    // partialeq
+    println!("{:?}", red == Color::Red);
+    println!("{:?}", rgba == Color::Rgba(255, 0, 0, 1.0));
+    println!("{:?}", hex == Color::Hex("#FF0000".to_string()));
+    println!(
+        "{:?}",
+        hsl == Color::Hsl {
+            h: 0,
+            s: 100,
+            l: 50
+        }
+    );
+
     // option
+    let x: Option<i32> = Some(5);
+    let y: Option<i32> = None;
+    println!("x: {:?}, y: {:?}", x, y);
     // result
+    let result: Result<i32, String> = Ok(5);
+    let error: Result<i32, String> = Err("Something went wrong".to_string());
+    println!("result: {:?}, error: {:?}", result, error);
 }
