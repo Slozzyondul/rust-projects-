8d51dfc5ded9635fbe08fd0dfaa9ed8ad7f655de
Slozzyondul <solomonondula@gmail.com>
2026-02-12 13:31:04 +0300

    normal enums and enums that takes in parameters

--- learning_project/src/bin/enum.rs
+++ learning_project/src/bin/enum.rs
@@ -1,3 +1,18 @@
 #![allow(unused)]
 
-fn main() {}
+enum Color {
+    Red,
+    Green,
+    Blue,
+    Rgba(u8, u8, u8, f32),
+    Hex(String),
+    Hsl { h: u8, s: u8, l: u8 },
+}
+
+fn main() {
+    // enum
+
+    // attributes - debug and partialeq
+    // option
+    // result
+}
