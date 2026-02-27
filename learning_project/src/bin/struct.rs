31c1c88f9a5754a84937458fabc6ec70b464c3cb
Slozzyondul <solomonondula@gmail.com>
2026-02-27 14:37:55 +0300

    creating simple structs

--- learning_project/src/bin/struct.rs
+++ learning_project/src/bin/struct.rs
@@ -1,3 +1,30 @@
 #![allow(unused)]
 
-fn main() {}
+//point struct
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+// 3d point struct
+struct Point3d {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+//netsted struct
+struct Circle {
+    center: Point,
+    radius: u32,
+}
+
+fn main() {
+
+    // create
+    // debug
+    // read
+    // shortcut
+    // copy fields
+    // update
+}
