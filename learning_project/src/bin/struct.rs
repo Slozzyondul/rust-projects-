23cb6d362023843950ad6df77a7b487d8eec9025
Slozzyondul <solomonondula@gmail.com>
2026-02-27 14:54:52 +0300

    acces 3d struct

--- learning_project/src/bin/struct.rs
+++ learning_project/src/bin/struct.rs
@@ -7,11 +7,7 @@ struct Point {
 }
 
 // 3d point struct
-struct Point3d {
-    x: i32,
-    y: i32,
-    z: i32,
-}
+struct Point3d(f32, f32, f32);
 
 //netsted struct
 struct Circle {
@@ -23,6 +19,9 @@ fn main() {
     // create
     let p = Point { x: 10.0, y: 20 };
     println!("point.x = {}, point.y = {}", p.x, p.y);
+
+    let p3d = Point3d(10.0, 20.0, 30.0);
+    println!("point3d = {}, {}, {}", p3d.0, p3d.1, p3d.2);
 
     // debug
     // read
