c81756a7b4d0f6386317ff7bee025d924fac9656
Slozzyondul <solomonondula@gmail.com>
2026-03-02 16:22:38 +0300

    normal if else loop

---
+++ learning_project/src/bin/match_statement.rs
@@ -0,0 +1,27 @@
+#![allow(unused)]
+
+fn main() {
+    let x = 1;
+    if x == 1 {
+        println!("one");
+    } else if x == 2 {
+        println!("two");
+    } else if x == 3 {
+        println!("three");
+    } else {
+        println!("something else");
+    }
+    // match
+
+    // multiple cases
+
+    // range
+
+    // @
+
+    // return value
+
+    // option
+
+    // result
+}
