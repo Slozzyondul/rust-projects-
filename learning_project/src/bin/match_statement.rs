b6c389d22d308e3425936050a820fcc46c268542
Slozzyondul <solomonondula@gmail.com>
2026-03-02 16:46:08 +0300

    match range and using @ symbol to identify the corect match

--- learning_project/src/bin/match_statement.rs
+++ learning_project/src/bin/match_statement.rs
@@ -11,6 +11,7 @@ fn main() {
     } else {
         println!("something else");
     }
+
     // match
     let x = 3;
     match x {
@@ -19,6 +20,7 @@ fn main() {
         3 => println!("three"),
         _ => println!("something else"),
     }
+
     // multiple cases
     let x = 5;
     match x {
@@ -28,8 +30,17 @@ fn main() {
     }
 
     // range
+    let x = 5;
+    match x {
+        1..=5 => println!("one or two or three or four or five"),
+        _ => println!("something else"),
+    }
 
     // @
+    match x {
+        i @ 1..=5 => println!("{}", i),
+        _ => println!("something else"),
+    }
 
     // return value
 
