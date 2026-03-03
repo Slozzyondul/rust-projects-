6418565a4fbe8178487363d2a66589581bf5e7a8
Slozzyondul <solomonondula@gmail.com>
2026-03-03 16:19:05 +0300

    handle error with result

--- learning_project/src/bin/error_handle.rs
+++ learning_project/src/bin/error_handle.rs
@@ -1,5 +1,18 @@
 #![allow(unused)]
 
+#[derive(Debug)]
+enum MathError {
+    DivisionByZero,
+    Other,
+}
+
+fn div(x: u32, y: u32) -> Result<u32, MathError> {
+    if y == 0 {
+        return Err(MathError::DivisionByZero);
+    }
+    Ok(x / y)
+}
+
 fn main() {
     //error
     //panic!("crash and burn");
@@ -13,5 +26,13 @@ fn main() {
     match x {
         Some(val) => println!("Value is {}", val),
         None => println!("Value is None"),
+    }
+
+    let x = 1;
+    let y = 19;
+    // result<T, E> = Ok(T) | Err(E)
+    match div(x, y) {
+        Ok(val) => println!("Value is {}", val),
+        Err(e) => println!("Error is {:?}", e),
     }
 }
