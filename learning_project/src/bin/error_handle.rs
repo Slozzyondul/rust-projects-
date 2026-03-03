f67cf3c70a4f98a738930a5f98d85977e17915ce
Slozzyondul <solomonondula@gmail.com>
2026-03-03 16:08:49 +0300

    handle error with option and match

--- learning_project/src/bin/error_handle.rs
+++ learning_project/src/bin/error_handle.rs
@@ -2,5 +2,16 @@
 
 fn main() {
     //error
-    panic!("crash and burn");
+    //panic!("crash and burn");
+
+    // option or result
+    let arr = [1, 2, 3];
+    //arr[10];
+
+    //option<&i32> = some(&i32) | None
+    let x: Option<&i32> = arr.get(2);
+    match x {
+        Some(val) => println!("Value is {}", val),
+        None => println!("Value is None"),
+    }
 }
