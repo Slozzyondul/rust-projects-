2e7e5f9936743d195527403af18e1fe23e4ea31d
Slozzyondul <solomonondula@gmail.com>
2026-03-05 15:03:08 +0300

    addition, subtraction, multiplication and division round down

--- learning_project/src/bin/operators.rs
+++ learning_project/src/bin/operators.rs
@@ -3,6 +3,15 @@
 fn main() {
     // +. -, *, /
 
+    let x: i32 = 1;
+    let y: i32 = 2;
+    println!("x + y = {}", x + y);
+    println!("x - y = {}", x - y);
+    println!("x * y = {}", x * y);
+
+    // in rust division of integers rounds down
+    println!("x / y = {}", x / y);
+
     // %  (remainder or mod operation)
 
     // literals
