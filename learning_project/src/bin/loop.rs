54658c305f1e6fe6c0739e48f5da9c021a51f9b7
Slozzyondul <solomonondula@gmail.com>
2026-03-23 16:28:41 +0300

    loop a range of ten values

---
+++ learning_project/src/bin/loop.rs
@@ -0,0 +1,21 @@
+#[allow(unused)]
+
+fn main() {
+    // loop
+    let mut i = 0;
+    loop {
+        println!("loop {i}");
+        if i == 10 {
+            break;
+        }
+        i += 1;
+    }
+    // while
+    // for loop
+    // for loop array
+    // usize and range
+    // for loop vector
+    // iter
+    // return value
+    // labels
+}
