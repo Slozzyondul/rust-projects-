1c0d3169cc7b85a89e87b71f4b716c07f7033f71
Slozzyondul <solomonondula@gmail.com>
2026-03-23 16:34:01 +0300

    use for to return 10 values

--- learning_project/src/bin/loop.rs
+++ learning_project/src/bin/loop.rs
@@ -17,6 +17,9 @@ fn main() {
         i += 1;
     }
     // for loop
+    for i in 0..=10 {
+        println!("for {i}");
+    }
     // for loop array
     // usize and range
     // for loop vector
